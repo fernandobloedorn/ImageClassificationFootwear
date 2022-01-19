2d3ec636441db78ec8a0204573ded4a0c030aaa0
Fernando Bloedorn <fernandobloedorn@gmail.com>
2022-01-19 15:51:54 -0300

    Add existing file

--- cnn.py
+++ cnn.py
@@ -150,3 +150,110 @@ class Train:
             save_best_only=True, save_weights_only=True,mode='auto')
         self.cbks = [checkpoint]
         self.model = model
+
+
+class Test:
+    '''One parameter model which is a keras model'''
+
+    def __init__(self,label):
+
+        self.master_classes=4
+        self.sub_classes=18
+
+        '''Three inputs to model for training, image, labels ,labels for teacher forcing'''
+        input_image = Input(shape=(150,200,3),name="InputImg")
+
+        #Layers are named to be same as VGG16
+
+        #will need to center and scale data.
+
+        #--- block 1 ---
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_image)
+        x = BatchNormalization()(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+        #--- block 2 ---
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+        x = BatchNormalization()(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+        #--- block 3 ---
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+        x = BatchNormalization()(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+        #--- block 4 ---
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+        x = BatchNormalization()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+        #--- block 5 masterCategory ---
+        x1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1_mas')(x)
+        x1 = BatchNormalization()(x1)
+        x1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2_mas')(x1)
+        x1 = BatchNormalization()(x1)
+        x1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3_mas')(x1)
+        x1 = BatchNormalization()(x1)
+
+        #--- masterCategory prediction branch ---
+        c_1_bch = Flatten(name='c1_flatten')(x1)
+        c_1_bch = Dense(256, activation='relu', name='c1_fc_mas')(c_1_bch)
+        c_1_bch = BatchNormalization()(c_1_bch)
+        c_1_bch = Dropout(0.5)(c_1_bch)
+        c_1_bch = Dense(256, activation='relu', name='c1_fc2_mas')(c_1_bch)
+        c_1_bch = BatchNormalization()(c_1_bch)
+        c_1_bch = Dropout(0.5)(c_1_bch)
+        c_1_pred = Dense(self.master_classes, activation='softmax', name='master_output')(c_1_bch)
+
+        #--- block 5 subCategory ---
+        x2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1_sub')(x)
+        x2 = BatchNormalization()(x2)
+        x2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2_sub')(x2)
+        x2 = BatchNormalization()(x2)
+        x2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3_sub')(x2)
+        x2 = BatchNormalization()(x2)
+
+        #--- coarse 2 branch ---
+        c_2_bch = Flatten(name='c2_flatten')(x2)
+        c_2_bch = Dense(1024, activation='relu', name='c2_fc_sub')(c_2_bch)
+        c_2_bch = BatchNormalization()(c_2_bch)
+        c_2_bch = Dropout(0.5)(c_2_bch)
+        c_2_bch = Dense(1024, activation='relu', name='c2_fc2_sub')(c_2_bch)
+        c_2_bch = BatchNormalization()(c_2_bch)
+        c_2_bch = Dropout(0.5)(c_2_bch)
+
+        #--- masterCategory conditioning for subCategory branch ---
+        c_1_condition = Dense(self.sub_classes, activation=None, use_bias=False, kernel_constraint=NonNegUnitNorm(),name='c_1_condition')(c_1_pred)
+        c_2_raw = Dense(self.sub_classes, activation='relu', name='c_2_raw')(c_2_bch)
+        preds_features = Add()([c_1_condition,c_2_raw])
+        c_2_pred = Softmax(name='sub_output')(preds_features)
+
+        model = Model(
+            inputs=[input_image],
+            outputs=[c_1_pred, c_2_pred],
+            name="Condition_CNN")
+
+        losses = {
+            "master_output": "categorical_crossentropy",
+            "sub_output": "categorical_crossentropy"
+        }
+
+        trainable_params= np.sum([K.count_params(w) for w in model.trainable_weights])
+        #trainable_params = tf.keras.backend.count_params(model.trainable_weights)
+        print("Trainable paramaters: "+str(trainable_params))
+
+        model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss=losses,
+                      metrics=['categorical_accuracy'])
+                      
+        checkpoint = ModelCheckpoint("./weights/"+label+"_best_weights.h5", monitor='val_loss', verbose=1,
+            save_best_only=True, save_weights_only=True,mode='auto')
+        self.cbks = [checkpoint]
+        self.model = model
